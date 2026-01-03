54acc2670ced8288b3fe33439b60916c58c00c74
Foteini Antoniou <t8230222@aueb.gr>
2026-01-03 20:51:03 +0200

    Create message.java

---
+++ javaclasses/message.java
@@ -0,0 +1,63 @@
+package
+
+import java.time.LocalDateTime;
+
+
+public class Message {
+  private int message_id;
+  private int sender_id;
+  private int trip_id;
+  private LocalDateTime send_date;
+
+  public Message() {}
+
+  public Message (int sender_id, int trip_id, String content, LocalDateTime send_date){
+    this.sender_id = sender_id;
+    this.trip_id = trip_id;
+    this.content = content;
+    this.send_date = send_date;
+  }
+
+  public int getMessage_id() {
+    return message_id;
+  }
+
+  public void setMessage_id (int message_id){
+    this.message_id =message_id;
+  }
+
+  public int getSender_id(){
+    return sender_id;
+  }
+
+  public void setSender_id (int sender_id){
+    this.sender_id =sender_id;
+  }
+
+  public int getTrip_id() {
+    return trip_id;
+  }
+
+  public void setTrip_id (int trip_id) {
+    this.trip_id = trip_id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent (String content) {
+    this.content = content;
+  }
+
+  public LocalDateTime getSend_date() {
+    return send_date;
+  }
+
+  public void setSend_date (LocalDateTime send_date) {
+    this.send_date= send_date;
+  }
+
+    
+}
+
